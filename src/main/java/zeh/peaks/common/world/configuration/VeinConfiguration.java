0c99d36ab319411050a2995323550f84bb2f29a3
zehmaria <10835511+zehmaria@users.noreply.github.com>
2024-05-07 01:17:09 -0300

    targets

--- src/main/java/zeh/peaks/common/world/configuration/VeinConfiguration.java
+++ src/main/java/zeh/peaks/common/world/configuration/VeinConfiguration.java
@@ -5,30 +5,29 @@ import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.world.level.levelgen.feature.configurations.FeatureConfiguration;
 import net.minecraft.world.level.levelgen.feature.stateproviders.BlockStateProvider;
 
+import java.util.List;
+
 
 public record VeinConfiguration(int extraRadius, int extraDeltaRadius,
-								BlockStateProvider replaceBottom, BlockStateProvider replaceCore,
-								BlockStateProvider replaceStone, BlockStateProvider replaceStoneWrapper,
-								BlockStateProvider replaceDeepslate, BlockStateProvider replaceDeepslateWrapper) implements FeatureConfiguration {
+								boolean topEnabled, float topMultiplier,
+								boolean bottomEnabled, float bottomMultiplier, BlockStateProvider bottomDeposit,
+								List<CylindricalSurface> surfaces) implements FeatureConfiguration {
 
 	public static final Codec<VeinConfiguration> CODEC = RecordCodecBuilder.create(config -> config.group(
 			Codec.INT.fieldOf("extra_radius").forGetter(VeinConfiguration::extraRadius),
 			Codec.INT.fieldOf("extra_delta_radius").forGetter(VeinConfiguration::extraDeltaRadius),
-			BlockStateProvider.CODEC.fieldOf("replace_bottom").forGetter(VeinConfiguration::replaceBottom),
-			BlockStateProvider.CODEC.fieldOf("replace_core").forGetter(VeinConfiguration::replaceCore),
-			BlockStateProvider.CODEC.fieldOf("replace_stone").forGetter(VeinConfiguration::replaceStone),
-			BlockStateProvider.CODEC.fieldOf("replace_stone_wrapper").forGetter(VeinConfiguration::replaceStoneWrapper),
-			BlockStateProvider.CODEC.fieldOf("replace_deepslate").forGetter(VeinConfiguration::replaceDeepslate),
-			BlockStateProvider.CODEC.fieldOf("replace_deepslate_wrapper").forGetter(VeinConfiguration::replaceDeepslateWrapper)
+
+			Codec.BOOL.fieldOf("top_enabled").forGetter(VeinConfiguration::topEnabled),
+			Codec.FLOAT.fieldOf("top_height_multiplier").forGetter(VeinConfiguration::topMultiplier),
+
+			Codec.BOOL.fieldOf("bottom_enabled").forGetter(VeinConfiguration::bottomEnabled),
+			Codec.FLOAT.fieldOf("bottom_height_multiplier").forGetter(VeinConfiguration::bottomMultiplier),
+			BlockStateProvider.CODEC.fieldOf("bottom_deposit").forGetter(VeinConfiguration::bottomDeposit),
+
+			Codec.list(CylindricalSurface.CODEC).fieldOf("cylindrical_surfaces").forGetter(VeinConfiguration::surfaces)
 		).apply(config, VeinConfiguration::new));
 
-    public int extraRadius() { return this.extraRadius; }
+	public int extraRadius() { return this.extraRadius; }
 	public int extraDeltaRadius() { return this.extraDeltaRadius; }
-	public BlockStateProvider replaceBottom() { return this.replaceBottom; }
-	public BlockStateProvider replaceCore() { return this.replaceCore; }
-	public BlockStateProvider replaceStone() { return this.replaceStone; }
-	public BlockStateProvider replaceStoneWrapper() { return this.replaceStoneWrapper; }
-	public BlockStateProvider replaceDeepslate() { return this.replaceDeepslate; }
-	public BlockStateProvider replaceDeepslateWrapper() { return this.replaceDeepslateWrapper; }
 
 }
--- src/main/java/zeh/peaks/common/world/feature/VeinFeature.java
+++ src/main/java/zeh/peaks/common/world/feature/VeinFeature.java
@@ -11,9 +11,9 @@ import net.minecraft.world.level.levelgen.feature.FeaturePlaceContext;
 import net.minecraft.world.level.WorldGenLevel;
 import net.minecraft.util.RandomSource;
 import net.minecraft.core.BlockPos;
-import net.minecraft.tags.BlockTags;
-import zeh.peaks.Peaks;
+import net.minecraft.world.level.levelgen.feature.configurations.OreConfiguration.TargetBlockState;
 import zeh.peaks.common.Configuration;
+import zeh.peaks.common.world.configuration.CylindricalSurface;
 import zeh.peaks.common.world.configuration.VeinConfiguration;
 
 public class VeinFeature extends Feature<VeinConfiguration> {
@@ -32,12 +32,7 @@ public class VeinFeature extends Feature<VeinConfiguration> {
         RandomSource _random = context.random();
         BlockPos _pos = context.origin();
 
-        BlockState _replaceBottom = _vein.replaceBottom().getState(_random, _pos);
-        BlockState _replaceCore = _vein.replaceCore().getState(_random, _pos);
-        BlockState _replaceStone = _vein.replaceStone().getState(_random, _pos);
-        BlockState _stoneWrapper = _vein.replaceStoneWrapper().getState(_random, _pos);
-        BlockState _replaceDeepslate = _vein.replaceDeepslate().getState(_random, _pos);
-        BlockState _deepslateWrapper = _vein.replaceDeepslateWrapper().getState(_random, _pos);
+        BlockState _replaceBottom = _vein.bottomDeposit().getState(_random, _pos);
 
         int count = 0;
         int _maxRadius = radius + _vein.extraRadius() + _random.nextInt(deltaRadius + _vein.extraDeltaRadius());
@@ -48,13 +43,12 @@ public class VeinFeature extends Feature<VeinConfiguration> {
         try {
             for (int i = -_maxRadius; i <= _maxRadius; i++) {
                 for (int j = -_maxRadius; j <= _maxRadius; j++) {
-                    int _dr = _maxRadius - _random.nextInt(deltaRadius + _vein.extraDeltaRadius());
+                    int _dr = _maxRadius - _random.nextInt(deltaRadius + _vein.extraDeltaRadius();
 
                     if (i * i + j * j <= _dr * _dr) {
                         _anchor.set(_pos.offset(0, _maxRadius + 1, 0));
                         boolean _extraBlocks = true;
-                        boolean _l1 = i * i + j * j < _dr * _dr / 13;
-                        boolean _l2 = i * i + j * j < _dr * _dr / 2;
+                        float _radius = Mth.sqrt((float) (i * i + j * j) / (_dr * _dr));
                         int _peak = (int) (Math.PI * (_dr * _dr - (i * i + j * j)) / _dr);
 
                         while (_anchor.getY() > -63) {
@@ -68,48 +62,49 @@ public class VeinFeature extends Feature<VeinConfiguration> {
                             int _y = SectionPos.sectionRelative(_offset.getY());
                             int _z = SectionPos.sectionRelative(_offset.getZ());
 
-                            if (_section.getBlockState(_x, _y, _z).is(BlockTags.OVERWORLD_CARVER_REPLACEABLES)) {
-                                if (_l1) {
-                                    _section.setBlockState(_x, _y, _z, _replaceCore, false);
-                                } else if (_section.getBlockState(_x, _y, _z).is(BlockTags.DEEPSLATE_ORE_REPLACEABLES)) {
-                                    _section.setBlockState(_x, _y, _z, _l2 ?
-                                            _replaceDeepslate : _deepslateWrapper, false);
-                                } else {
-                                    _section.setBlockState(_x, _y, _z, _l2 ?
-                                            _replaceStone : _stoneWrapper, false);
-                                }
-                                count++;
+                            BlockState state = _section.getBlockState(_x, _y, _z);
+                            for (CylindricalSurface surface : _vein.surfaces()) {
+                                if (_radius >= surface.minRadius() && _radius <= surface.maxRadius()) {
+                                    for (TargetBlockState targetState : surface.targets()) {
+                                        if (canPlaceOre(state, _random, targetState)) {
+                                            _section.setBlockState(_x, _y, _z, targetState.state, false);
+                                            ++count;
 
-                                if (_extraBlocks) {
-                                    for (int m = 1; m <= _peak; m++) {
-                                        _offset.setWithOffset(_anchor, i, m, j);
-                                        if (!_level.ensureCanWrite(_offset)) continue;
-                                        LevelChunkSection _top = _bulk.getSection(_offset);
-                                        if (_top == null) continue;
-                                        int _xx = SectionPos.sectionRelative(_offset.getX());
-                                        int _yy = SectionPos.sectionRelative(_offset.getY());
-                                        int _zz = SectionPos.sectionRelative(_offset.getZ());
-
-                                        _top.setBlockState(_xx, _yy, _zz, _l1 ? _replaceCore :
-                                                (_l2 ? _replaceStone : _stoneWrapper), false);
-                                        count++;
+                                            if (_extraBlocks && _vein.topEnabled()) {
+                                                for (int m = 0; m <= _peak * _vein.topMultiplier(); m++) {
+                                                    _offset.setWithOffset(_anchor, i, m, j);
+                                                    if (!_level.ensureCanWrite(_offset)) continue;
+                                                    LevelChunkSection _top = _bulk.getSection(_offset);
+                                                    if (_top == null) continue;
+                                                    int _xx = SectionPos.sectionRelative(_offset.getX());
+                                                    int _yy = SectionPos.sectionRelative(_offset.getY());
+                                                    int _zz = SectionPos.sectionRelative(_offset.getZ());
+                                                    _top.setBlockState(_xx, _yy, _zz, targetState.state, false);
+                                                    count++;
+                                                }
+                                                _extraBlocks = false;
+                                            }
+                                            break;
+                                        }
                                     }
-                                    _extraBlocks = false;
+                                    break;
                                 }
                             }
                         }
 
-                        for (int m = 1; m <= _peak / 2; m++) {
-                            _offset.setWithOffset(_anchor, i, m, j);
-                            if (!_level.ensureCanWrite(_offset)) continue;
-                            LevelChunkSection _bottom = _bulk.getSection(_offset);
-                            if (_bottom == null) continue;
-                            int _xx = SectionPos.sectionRelative(_offset.getX());
-                            int _yy = SectionPos.sectionRelative(_offset.getY());
-                            int _zz = SectionPos.sectionRelative(_offset.getZ());
+                        if (_vein.bottomEnabled()) {
+                            for (int m = 1; m <= _peak * _vein.bottomMultiplier(); m++) {
+                                _offset.setWithOffset(_anchor, i, m, j);
+                                if (!_level.ensureCanWrite(_offset)) continue;
+                                LevelChunkSection _bottom = _bulk.getSection(_offset);
+                                if (_bottom == null) continue;
+                                int _xx = SectionPos.sectionRelative(_offset.getX());
+                                int _yy = SectionPos.sectionRelative(_offset.getY());
+                                int _zz = SectionPos.sectionRelative(_offset.getZ());
 
-                            _bottom.setBlockState(_xx, _yy, _zz, _l2 ? _replaceBottom : _deepslateWrapper, false);
-                            count++;
+                                _bottom.setBlockState(_xx, _yy, _zz, _replaceBottom, false);
+                                count++;
+                            }
                         }
                     }
                 }
@@ -125,6 +120,10 @@ public class VeinFeature extends Feature<VeinConfiguration> {
 
         _bulk.close();
         return count > 0;
+    }
+
+    private static boolean canPlaceOre(BlockState state, RandomSource random, TargetBlockState targetState) {
+        return targetState.target.test(state, random);
     }
 
 }
